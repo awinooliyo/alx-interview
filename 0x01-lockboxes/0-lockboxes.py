1fac212110e1bd5b0f3fbac42f883d07644ee6a3
awinooliyo <erick.otieno89@gmail.com>
2024-07-05 21:08:54 +0300

    locboxes pep 8 compliance

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,15 +2,18 @@
 """
 Module: lockboxes
 
-Contains the function canUnlockAll to determine if all lockboxes can be opened.
+Contains the function canUnlockAll to determine
+if all lockboxes can be opened.
 """
+
 
 def canUnlockAll(boxes):
     """
     Determines if all lockboxes can be opened starting from the first box.
 
     Args:
-    - boxes (list of list of int): List of lockboxes where each box (index) contains a list of keys (integers).
+    - boxes (list of list of int): List of lockboxes
+    where each box (index) contains a list of keys (integers).
 
     Returns:
     - bool: True if all lockboxes can be opened, False otherwise.
@@ -23,17 +26,16 @@ def canUnlockAll(boxes):
     n = len(boxes)
     if n == 0:
         return False
-    
+
     visited = set()
     visited.add(0)  # Start from the first box which is unlocked
     queue = [0]     # Queue to manage boxes to be opened next
-    
+
     while queue:
         current_box = queue.pop(0)
         for key in boxes[current_box]:
             if key < n and key not in visited:
                 visited.add(key)
                 queue.append(key)
-    
-    return len(visited) == n
 
+    return len(visited) == n
